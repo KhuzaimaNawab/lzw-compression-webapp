d0860526cdc2d72914f737bf4a83152fc1d823aa
Khuzaima Nawab <63063076+KhuzaimaNawab@users.noreply.github.com>
2023-04-29 02:01:31 +0500

    Fix scrolling issue and made it responsive

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => SelectFileProvider(),
       child: const MaterialApp(
+        title: 'LZW Compression',
         debugShowCheckedModeBanner: false,
         home: HomePage(),
       ),
--- lib/presentation/screen/home_page.dart
+++ lib/presentation/screen/home_page.dart
@@ -19,165 +19,198 @@ class HomePage extends StatelessWidget {
 
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Padding(
-        padding:
-            EdgeInsets.only(left: size.width * 0.30, right: size.width * 0.30),
-        child: SizedBox(
-          width: size.width,
-          height: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset('assets/images/logo.png', width: 70, height: 70),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  const Text(
-                    'LZW Data Compression',
-                    style: TextStyle(fontSize: 30, fontFamily: 'Sigmar'),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              RichText(
-                text: TextSpan(
-                  text: 'This ',
-                  style: subTitleStype,
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Online Text Compresser ',
-                      style: subTitleStype.copyWith(
-                        fontWeight: FontWeight.bold,
+      body: SizedBox(
+        width: size.width,
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+                left: size.width < 900 ? size.width * 0.10 : size.width * 0.30,
+                right:
+                    size.width < 900 ? size.width * 0.10 : size.width * 0.30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset('assets/images/logo.png',
+                        width: size.width < 400 ? 0 : 70,
+                        height: size.width < 400 ? 0 : 70),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Expanded(
+                      child: Text(
+                        'LZW Data Compression',
+                        style: TextStyle(fontSize: 30, fontFamily: 'Sigmar'),
                       ),
-                    ),
-                    const TextSpan(
-                      text:
-                          "compresses text. This is loseless compression meaning your data will not be lost. Your file will be shrinked as low as possible. The compressed file will be unuseable until it's decompressed. Your File extension will be ",
-                      style: subTitleStype,
-                    ),
-                    TextSpan(
-                      text: ".lzw. ",
-                      style: subTitleStype.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const TextSpan(
-                      text:
-                          "You can decompress your file on this website only.",
-                      style: subTitleStype,
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                "NOTE: PLEASE UPLOAD A FILE THAT HAVE ATLEAST 2-3 PARAGRAPH OTHERWISE THE COMPRESSION WILL NOT WORK.",
-                style: subTitleStype.copyWith(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              const Text(
-                'Select a file to compress (The file should be in .txt extension)',
-                style: subTitleStype,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SelectFileCompressBtn(
-                    extension: 'txt',
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  const CompressBtn(),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Consumer<SelectFileProvider>(
-                builder: (context, value, _) {
-                  return Visibility(
-                    visible: value.compressedFileName != null,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Your File Name is: ',
-                          style: subTitleStype,
+                const SizedBox(
+                  height: 40,
+                ),
+                RichText(
+                  text: TextSpan(
+                    text: 'This ',
+                    style: subTitleStype,
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Online Text Compresser ',
+                        style: subTitleStype.copyWith(
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          value.compressedFileName ?? '',
-                          style: subTitleStype.copyWith(
-                            fontWeight: FontWeight.bold,
+                      ),
+                      const TextSpan(
+                        text:
+                            "compresses text. This is loseless compression meaning your data will not be lost. Your file will be shrinked as low as possible. The compressed file will be unuseable until it's decompressed. Your File extension will be ",
+                        style: subTitleStype,
+                      ),
+                      TextSpan(
+                        text: ".lzw. ",
+                        style: subTitleStype.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const TextSpan(
+                        text:
+                            "You can decompress your file on this website only.",
+                        style: subTitleStype,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "NOTE: PLEASE UPLOAD A FILE THAT HAVE ATLEAST 2-3 PARAGRAPH OTHERWISE THE COMPRESSION WILL NOT WORK.",
+                  style: subTitleStype.copyWith(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text(
+                  'Select a file to compress (The file should be in .txt extension)',
+                  style: subTitleStype,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                size.width < 450
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SelectFileCompressBtn(
+                            extension: 'txt',
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(height: 60),
-              const Text(
-                'Select a file to decompress (The file should be in .lzw extension)',
-                style: subTitleStype,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SelectFileDecompressBtn(
-                    extension: 'lzw',
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  const DecompressBtn(),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Consumer<SelectFileProvider>(
-                builder: (context, value, _) {
-                  return Visibility(
-                    visible: value.decompressedFileName != null,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Your File Name is: ',
-                          style: subTitleStype,
-                        ),
-                        Text(
-                          value.decompressedFileName ?? '',
-                          style: subTitleStype.copyWith(
-                            fontWeight: FontWeight.bold,
+                          const SizedBox(
+                            height: 20,
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ],
+                          const CompressBtn(),
+                        ],
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SelectFileCompressBtn(
+                            extension: 'txt',
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const CompressBtn(),
+                        ],
+                      ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Consumer<SelectFileProvider>(
+                  builder: (context, value, _) {
+                    return Visibility(
+                      visible: value.compressedFileName != null,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Your File Name is: ',
+                            style: subTitleStype,
+                          ),
+                          Text(
+                            value.compressedFileName ?? '',
+                            style: subTitleStype.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 60),
+                const Text(
+                  'Select a file to decompress (The file should be in .lzw extension)',
+                  style: subTitleStype,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                size.width < 450
+                    ? Column(
+                        children: [
+                          SelectFileDecompressBtn(
+                            extension: 'lzw',
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const DecompressBtn(),
+                        ],
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SelectFileDecompressBtn(
+                            extension: 'lzw',
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const DecompressBtn(),
+                        ],
+                      ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Consumer<SelectFileProvider>(
+                  builder: (context, value, _) {
+                    return Visibility(
+                      visible: value.decompressedFileName != null,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Your File Name is: ',
+                            style: subTitleStype,
+                          ),
+                          Text(
+                            value.decompressedFileName ?? '',
+                            style: subTitleStype.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
